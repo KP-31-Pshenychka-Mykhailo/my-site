c14138235c6f61347e0e64930671c8edd4c47832
Mihail.P <144612697+KP-31-Pshenychka-Mykhailo@users.noreply.github.com>
2024-05-15 21:06:31 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://KP-31-Pshenychka-Mykhailo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/lab2-oksm-mysite/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'KP-31-Pshenychka-Mykhailo', // Usually your GitHub org/user name.
+  projectName: 'lab2-oksm-mysite', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
